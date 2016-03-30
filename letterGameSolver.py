c3b4a3ffaa40cecc05c0b0611c0a4d99fefbfc55
YesMAnKablam <conorkenny183@gmail.com>
2016-03-30 13:45:39 +0100

    Forgot to add final code

---
+++ letterGameSolver.py
@@ -0,0 +1,46 @@
+import time
+start_time = time.time()
+
+with open('newDic.txt', 'r') as fileopen:
+    values = [line.strip() for line in fileopen]
+
+
+# Setting up lists here use of generating your anagram
+import random
+anagram = []
+vowels = ['a','e','i','o','u']
+consonant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','x','z','w','y']
+
+
+# Loops here will append the required vowels and consonants to the anagram
+# It then fills the remaining space with either consonants or vowels
+for i in range (0,3):
+    anagram.append(random.choice(vowels))
+for i in range (0,4):
+    anagram.append(random.choice(consonant))
+for i in range (0,2):
+    anagram.append(random.choice(vowels + consonant))
+
+
+# Imports the shuffle function and jumbles the anagram so that it doesn't really look like it follows a format
+from random import shuffle
+shuffle(anagram)
+print (''.join(anagram))
+
+
+# After displaying the anagram, this alpabetises it, allow for some pretty fast searching
+
+#anagram = ['a','u','c','t','i','o','n','e','d']
+sanagram = (''.join(sorted(anagram)))
+
+def solver(word):
+    answers = []
+    for item in values:
+        if (''.join(sorted(item))) in word:
+            print (item)
+            break
+
+if __name__=='__main__':
+    from timeit import Timer
+    t = Timer(lambda: solver(sanagram))
+    print (t.timeit(number=1))
